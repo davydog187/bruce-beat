0be8000da72ddb025660900311601399e444ae11
Dave Lucia <dlucia@bloomberg.net>
2015-03-28 13:42:03 -0400

    [DL] Create a simple server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+var http = require("http");
+var fs = require("fs");
+
+var BRUCE_BEAT = "./html/bruce-beat.html";
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    response.end(fs.readFileSync(BRUCE_BEAT));
+}).listen(8080, "127.0.0.1");
+
+console.log("bruce beat is running, bitches");
